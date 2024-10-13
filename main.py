588bf1ef29ee46f2735caa9e491192362278c357
Iga Borowska <iga.borowska@gmail.com>
2024-10-13 21:42:43 +0200

    zadanie 0

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
+'''
+Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
 wyraz=input()
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
+W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
+To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
 wyraz=wyraz.strip()
 
-#Wydruk na ekranie (w konsoli)
+Wydruk na ekranie (w konsoli)
 print ('Ten wyraz został wprowadzony:', wyraz)
+'''
+
+
+from collections import defaultdict
+import string
+import re
+
+n = int(input("Podaj liczbę dokumentów: ").strip())
+dokumenty = [input("Dokument: ").strip().lower() for _ in range(n)]
+m = int(input("Ile wyrazów do wyszukania?: ").strip())
+wyrazy = [input("Wyrazy: ").strip().lower() for _ in range(m)]
+
+#Znaki specjalne
+tlumacz = str.maketrans('', '', string.punctuation)
+dokumenty = [' '.join(re.split(r'\s+', dokument.translate(tlumacz))) for dokument in dokumenty]
+
+
+licznik = [defaultdict(int) for _ in range(n)]
+
+for i, dokument in enumerate(dokumenty):
+    for slowo in dokument.split():
+        licznik[i][slowo] += 1
+
+for zapytanie in wyrazy:
+    wynik = []
+
+    for i in range(n):
+        if zapytanie in licznik[i]:
+            wynik.append((licznik[i][zapytanie], i))
+
+    wynik.sort(key=lambda x: (-x[0], x[1]))
+    wyjście = [indeks_dok for liczba, indeks_dok in wynik]
+
+    print(wyjście)
+
